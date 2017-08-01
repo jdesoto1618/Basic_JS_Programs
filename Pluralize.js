04f667749a0427719537d6fc2abfc420589364d8
Joe DeSoto <modulusman@gmail.com>
2017-07-31 21:08:25 -0700

    Changed some switch case alerts. Returned the noun if number is less than 2

--- Pluralize.js
+++ Pluralize.js
@@ -1,4 +1,11 @@
 function pluralizer(num, noun) {
+  //The program only pluralizes the words given in the switch statements. It won't capture all cases, but it does include a default case for non irregular plural forms.
+
+  //Add check for empty second parameter
+  if (typeof(noun) === "undefined") {
+    return "You must specify a noun to use this program!"
+  }
+
   if (num > 1) {
     switch (noun.toLowerCase()) {
       case "sheep":
@@ -8,12 +15,12 @@ function pluralizer(num, noun) {
         alert(num + " of these are called geese.");
         break;
       case "child":
-        alert(num + " of these are called children.");
+        alert("The plural of child is children.");
         break;
       default:
-        alert(num + " " + noun + "s");
+        alert(noun + " pluralized is: " + noun + "s");
     }
   } else {
-    alert(noun + " is singular.");
+    return noun;
   }
 }

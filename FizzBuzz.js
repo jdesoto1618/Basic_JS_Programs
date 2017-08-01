00d1fed8ed75b026ceaebba87c590717e6effc7d
Joe DeSoto <modulusman@gmail.com>
2017-07-31 20:27:29 -0700

    Added comments, changed starting number to 0

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,11 +1,15 @@
-for(var i=1; i <=100; i++) {
-  if (i%15 === 0) {
+for(var i=0; i <=100; i++) {
+//The case when a 3 and 5 both divide a number. Handle this case first, so that it's not skipped over in later else if statements. Used 15 since 3 and 5 divide a number iff 15 divides that number.
+
+  if (i%15 == 0) {
     console.log("FizzBuzz");
-  } else if (i%5 === 0) {
+  } else if (i%5 == 0) {
     console.log("Buzz");
-  } else if (i%3 === 0) {
+  } else if (i%3 == 0) {
     console.log("Fizz");
   } else {
+
+//If a number is not divisible by any of the above cases, just print the number.
     console.log(i);
   }
 }

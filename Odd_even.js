89b07a0da34a980e1517aac6286c7fff4d6ce705
Joe DeSoto <modulusman@gmail.com>
2017-07-31 20:53:41 -0700

    Added commenting, changed starting condition in the for loop to include 0

--- Odd_even.js
+++ Odd_even.js
@@ -1,4 +1,5 @@
 function oddEven () {
+  //Function can start by testing 0. By definition, 0 is even. The program only tests the numbers 0 through 15 inclusive for odd or even. That is, if 2 is a factor, it's even. The program will list all the numbers in the range given and display the odd/even status.
   for (var i=0; i<=15; i++) {
     if (i%2 === 0) { //even cases
       console.log(i + " is an even number");

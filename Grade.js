ee4574f8be4afc1e332dd0bbb7e826d3345a4829
Joe DeSoto <modulusman@gmail.com>
2017-07-31 20:30:44 -0700

    Added comments. Program is basic but looks ok

--- Grade.js
+++ Grade.js
@@ -1,5 +1,5 @@
 function assignGrade (score) {
-  switch (true) { //Cool trick to activate a switch statement. This will run unless false
+  switch (true) { //Cool trick to activate a switch statement. This will always run, and can be used when there is no apparent starting case for the switch to start. Use true for this. If you don't have the break statements, it may allow the program to display the other values. 
     case (score >= 90):
       alert("A");
       break;

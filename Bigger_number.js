e6b574687af898c26406d9379cda19b50f916892
Joe DeSoto <modulusman@gmail.com>
2018-05-16 21:57:34 -0700

    cleans up bigger number function, adds commenting

--- Bigger_number.js
+++ Bigger_number.js
@@ -1,21 +1,26 @@
-//This function won't run until called
-function numbers (num1, num2) {
+function compareTwoNumbers (num1, num2) {
+  // store parameter values as variables
+  var p1 = num1;
+  var p2 = num2;
+  // sanity check for empty parameters
+  if(typeof(p1) === 'undefined' || typeof(p2) === 'undefined') {
+    console.log('One or more function inputs are missing!');
+    // end function when this if statement runs
+    return;
+    // check for string input
+  } else if(typeof(p1) === 'string' || typeof(p2) === 'string') {
+    // character escaped with \ in order to use the single quote in the log statement
+    console.log('Please don\'t use words or empty quotes');
+    // end function when this if statement runs
+    return;
+  } // ends typeof if statement check
+  // compare values if they're numbers
   if (num1 > num2) {
     alert("The two numbers are " + num1 + " and " + num2 + ". " + num1 + " is greater than " + num2);
-    return num1;
-
-//If the numbers are the same
+  // if the numbers are the same
   } else if (num1 == num2) {
       alert("The two numbers are equal.");
-      return num1
-
-//If the second number is not passed in
-  } else if (typeof(num2) === 'undefined') {
-      alert("One or more of the numbers was not specified in the function call! Try again.")
   } else {
       alert("The two numbers are " + num1 + " and " + num2 + ". " + num2 + " is greater than " + num1);
-      return num2;
-  }
-}
-
-//The function doesn't account for string input. It also breaks if num1 is not specified in the function call, with no apparent way to account for that. It breaks immediately in this case without executing any code. 
+  } // ends number comparator if
+} // ends compareTwoNumbers function
